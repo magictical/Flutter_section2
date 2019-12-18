0d241d95416bf88ab2310c71272965d673263363
IL KWON KIM <magictical@gmail.com>
2019-12-18 15:57:24 +0900

    split files and refactoring widget and models

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import './transaction.dart';
-import 'package:intl/intl.dart';
+import './widget/user_transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,24 +16,9 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transaction = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly eateries ',
-      amount: 15.99,
-      date: DateTime.now(),
-    ),
-  ];
-
   // String titleInput;
   // String amountInput;
-// instead use controller!
+  // instead use controller!
   final titleController = TextEditingController();
   final amountController = TextEditingController();
 
@@ -56,78 +40,7 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            elevation: 5,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(hintText: 'Title'),
-                    // onChanged: (val) => titleInput = val,
-                    // instead use controller
-                    controller: titleController,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(hintText: 'Amount'),
-                    // onChanged: (val) => amountInput = val,
-                    // instead use controller
-                    controller: amountController,
-                  ),
-                  FlatButton(
-                    child: Text('Add Transaction'),
-                    textColor: Colors.purple,
-                    onPressed: () {
-                      print(titleController.text);
-                      print(amountController.text);
-                    },
-                  )
-                ],
-              ),
-            ),
-          ),
-          Column(
-            children: transaction.map((transaction) {
-              return Card(
-                  child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      ),
-                    ),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      '\$ ${transaction.amount}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple,
-                      ),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        transaction.title,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 18),
-                      ),
-                      Text(
-                        DateFormat('yyyy MM/dd').format(transaction.date),
-                        style: TextStyle(color: Colors.grey),
-                      )
-                    ],
-                  )
-                ],
-              ));
-            }).toList(),
-          )
+          UserTransaction(),
         ],
       ),
     );
---
+++ lib/widget/new_transaction.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(hintText: 'Title'),
+              // onChanged: (val) => titleInput = val,
+              // instead use controller
+              controller: titleController,
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: 'Amount'),
+              // onChanged: (val) => amountInput = val,
+              // instead use controller
+              controller: amountController,
+            ),
+            FlatButton(
+              child: Text('Add Transaction'),
+              textColor: Colors.purple,
+              onPressed: () {
+                print(titleController.text);
+                print(amountController.text);
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
