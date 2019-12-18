8a560e74c9ac92d74ff47f96f22d7b1aab75edbd
IL KWON KIM <magictical@gmail.com>
2019-12-18 11:49:09 +0900

    styling text widget

--- lib/main.dart
+++ lib/main.dart
@@ -57,15 +57,35 @@ class MyHomePage extends StatelessWidget {
                   Container(
                     margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                     decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black, width: 2)),
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(10),
                     child: Text(
                       transaction.amount.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
                     ),
                   ),
-                  Column(children: <Widget>[
-                    Text(transaction.title),
-                    Text(transaction.date.toString())
-                  ])
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        transaction.title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                      Text(
+                        transaction.date.toString(),
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  )
                 ],
               ));
             }).toList(),
