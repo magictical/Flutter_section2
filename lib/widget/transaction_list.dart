3631ed43c2acb897ad4110b3ce33cc04e007caeb
IL KWON KIM <magictical@gmail.com>
2019-12-18 16:54:55 +0900

    aplying SingleChildScrollView

--- lib/widget/transaction_list.dart
+++ lib/widget/transaction_list.dart
@@ -9,45 +9,52 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((transaction) {
-        return Card(
-            child: Row(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.purple,
-                  width: 2,
-                ),
-              ),
-              padding: EdgeInsets.all(10),
-              child: Text(
-                '\$ ${transaction.amount}',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.purple,
-                ),
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      // 특정 구역만 스크롤이 가능해진다 차트 영역(상단)은 고정됨
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: transactions.map((transaction) {
+            return Card(
+                child: Row(
               children: <Widget>[
-                Text(
-                  transaction.title,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    ),
+                  ),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '\$ ${transaction.amount}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                    ),
+                  ),
                 ),
-                Text(
-                  DateFormat('yyyy MM/dd').format(transaction.date),
-                  style: TextStyle(color: Colors.grey),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transaction.title,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    Text(
+                      DateFormat('yyyy MM/dd').format(transaction.date),
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
                 )
               ],
-            )
-          ],
-        ));
-      }).toList(),
+            ));
+          }).toList(),
+        ),
+      ),
     );
   }
 }
