885ad16b780672241b649ade4a834d15dbd48e29
IL KWON KIM <magictical@gmail.com>
2019-12-20 13:47:18 +0900

    fixed name typo

--- lib/widget/chart_bar.dart
+++ lib/widget/chart_bar.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-class CharBar extends StatelessWidget {
+class ChartBar extends StatelessWidget {
   final String label;
   final double spendingAmount;
   final double spendingPctOfTotal;
 
-  CharBar(this.label, this.spendingAmount, this.spendingPctOfTotal);
+  ChartBar(this.label, this.spendingAmount, this.spendingPctOfTotal);
 
   @override
   Widget build(BuildContext context) {
