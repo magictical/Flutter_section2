5723be5d156daaac3b6a13c24ee8cf9b6f123f8d
IL KWON KIM <magictical@gmail.com>
2019-12-25 22:56:23 +0900

    split pick date button file from main to apdative_button.dart

--- lib/widget/new_transaction.dart
+++ lib/widget/new_transaction.dart
@@ -1,7 +1,7 @@
-import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'adaptive_button.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function transactionHandler;
@@ -98,28 +98,7 @@ class _NewTransactionState extends State<NewTransaction> {
                             : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
                       ),
                     ),
-                    Platform.isIOS
-                        ? CupertinoButton(
-                            child: Text(
-                              'Choose Date',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            onPressed: () {
-                              _presentDatePicker();
-                            })
-                        : FlatButton(
-                            textColor: Theme.of(context).primaryColor,
-                            child: Text(
-                              'Choose Date',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            onPressed: () {
-                              _presentDatePicker();
-                            }),
+                    AdaptiveFlatButton('Choose Date', _presentDatePicker)
                   ],
                 ),
               ),
